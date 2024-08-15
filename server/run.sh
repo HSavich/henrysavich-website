2fb570526b8307bdf16e04418ec5df3201fe4aaa
HSavich <henrysavich@Henrys-MacBook-Pro.local>
2024-08-14 23:47:14 -0500

    changed run file to not start nginx if nginx is already running.

--- server/run.sh
+++ server/run.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
-script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-conf_path="${script_path}/nginx/nginx.conf"
-echo $conf_path
-nginx -c $conf_path
+grep_count=$(ps aux | grep "master process nginx" | wc -l | tr -d " ")
+if [[ "$grep_count" == 1 ]]; then
+    echo "starting nginx"
+    script_path=$( cd "${0:a:h}" ; pwd -P )
+    conf_path="${script_path}/nginx/nginx.conf"
+    nginx -c $conf_path
+fi
+if [[ "$grep_count" > 1 ]]; then
+    echo "Error: process with title "master process nginx" already running"
+fi
+echo "end"
