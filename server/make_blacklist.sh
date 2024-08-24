1fb877bd9246e65c533782b30dbb20d733d40c1a
HSavich <hhsavich@gmail.com>
2024-08-24 02:56:32 +0000

    test commit

--- server/make_blacklist.sh
+++ server/make_blacklist.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#test comment
 maybelist=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /var/log/nginx/access.log | sort | uniq)
 whitelist=$(cat /var/log/nginx/access.log | grep "fifteen" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq)
 blacklist=$(echo "$maybelist $whitelist" | tr " " "\n" | sort | uniq -u) 
