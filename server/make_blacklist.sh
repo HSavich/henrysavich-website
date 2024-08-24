53c4833c49e063fc7b04ebb17ae3bb15a808780f
HSavich <hhsavich@gmail.com>
2024-08-24 02:49:28 +0000

    added print statements to make_blacklist.sh

--- server/make_blacklist.sh
+++ server/make_blacklist.sh
@@ -2,7 +2,11 @@
 maybelist=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' /var/log/nginx/access.log | sort | uniq)
 whitelist=$(cat /var/log/nginx/access.log | grep "fifteen" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq)
 blacklist=$(echo "$maybelist $whitelist" | tr " " "\n" | sort | uniq -u) 
+echo "Number of IPs that have visited the IP address:"
 echo $maybelist | wc -w
+echo "Number of IPs that have visited the fifteen puzzle"
 echo $whitelist | wc -w
+echo "Number of IPs that have visited the IP address but not the fifteen puzzle"
 echo $blacklist | wc -w
-echo $blacklist | tr " " "\n"  > bash_blacklist.txt
+echo $whitelist | tr " " "\n" > ip_whitelist.txt
+echo $blacklist | tr " " "\n"  > ip_blacklist.txt
