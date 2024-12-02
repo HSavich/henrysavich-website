ccf0816a9417a8b1e7ef96895f5c17a4e4ac8e04
HSavich <hhsavich@gmail.com>
2024-12-02 08:11:05 -0500

    Added brown blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-blue": "#3399ee",
+        "theme-brown": "#44332a",
+        "theme-lightgray": "#e9e9e9",
+        "theme-mediumgray": "#bbbbbb",
+      },
+    },
   },
   plugins: [],
 }
